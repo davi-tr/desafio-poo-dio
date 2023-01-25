76d2d5ef8f50c371cb3ef34d35a2d75de6ebc14a
Davi Trajano <davitrajano473@gmail.com>
2023-01-24 23:30:40 -0300

    Aula 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Javinha");
+        curso1.setDescricao("Aulas Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Javão");
+        curso2.setDescricao("Varias Aulas Java");
+        curso2.setCargaHoraria(20);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Aula");
+        mentoria1.setDescricao("Ajuda com java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
